b8589966f1a6c7b5a3a1fc7275ebe0c1caf62581
JARED SPENCER <jareddespencer@gmail.com>
2017-05-04 12:12:32 -0700

    added isPermutation implementation using library; reduces time complexity to O(2N)

--- Arrays & Strings/stringIsPermutation.js
+++ Arrays & Strings/stringIsPermutation.js
@@ -10,6 +10,21 @@ const isPermutation = (string1, string2) => {
   return string1.sort() === string2.sort();
 }
 
-const isPermutation = (string1, sting2) => {
-  
+const isPermutation = (string1, string2) => {
+  let library = {};
+  for (let i = 0; i < string1.length; i++) {
+    library[string1.charAt(i)] = library[string1.charAt(i)] || 0;
+    library[string1.charAt(i)]++;
+  }
+  for (let i = 0; i < string2.length; i++) {
+    if (!library[string2.charAt(i)]) {
+      return false;
+    } else {
+      library[string2.charAt(i)]--;
+    }
+    if (library[string2.charAt(i)] === 0) {
+      delete library[string2.charAt(i)];
+    }
+  }
+  return Object.keys(library).length <= 0;
 }
