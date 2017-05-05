56a98434add2dca06b196e7bf7c421a4826bfbd7
JARED SPENCER <jareddespencer@gmail.com>
2017-05-05 16:17:25 -0700

    finsihed fist two of three portions of oneAway

---
+++ Arrays & Strings/stringsDifferentByOneEdit.js
@@ -0,0 +1,19 @@
+// One Away: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
+
+const oneAway = (string1, string2) => {
+  if (string1 === string2) {
+    return true;
+  }
+  if (string1.length === string2.length) {
+    let flag = 0;
+    for (let i = 0; i < string1.length; i++) {
+      if (string1.charAt(i) !== string2.charAt(i)) {
+        flag++;
+      }
+      if (flag > 1) {
+        return false;
+      }
+    }
+  }
+  
+}
