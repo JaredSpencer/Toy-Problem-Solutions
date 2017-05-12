14ff2fe2656ddc9709552ad90a32555f3d153b50
JARED SPENCER <jareddespencer@gmail.com>
2017-05-11 20:33:08 -0700

    added partition. Still building. Need to swap lower values to second half of list, and vice versa.

---
+++ LinkedLists/partition.js
@@ -0,0 +1,22 @@
+// Partition: Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x. lf x is contained within the list, the values of x only need to be after the elements less than x (see below). The partition element x can appear anywhere in the "right partition"; it does not need to appear between the left and right partitions.
+
+const partition = (list, value) => {
+  let node = list.head;
+  let head = list.head;
+  while (node) {
+    if (node.value === value) {
+      break;
+    } else {
+      node = node.next
+    }
+  }
+  while (head !== node) {
+    if (head.value > node.value) {
+      let temp = head;
+      temp.next = node.next;
+      node.next = temp;
+      // now account for having removed it from the front half of the list. Will need to use head.next since you're removing items.
+    }
+  }
+  // while loop doing the same thing but for the second half of the list.
+}
