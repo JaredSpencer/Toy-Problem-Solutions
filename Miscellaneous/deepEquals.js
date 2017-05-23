b3928fe565305eb248313dd35f373545ad7b11d4
JARED SPENCER <jareddespencer@gmail.com>
2017-05-23 16:28:24 -0700

    added allAnagrams and deepEquals for objects. Testing deepEquals.

---
+++ Miscellaneous/deepEquals.js
@@ -0,0 +1,39 @@
+/**
+  * Write a function that, given two objects, returns whether or not the two
+  * are deeply equivalent--meaning the structure of the two objects is the
+  * same, and so is the structure of each of their corresponding descendants.
+  *
+  * Examples:
+  *
+  * deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
+  * deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
+  *
+  * don't worry about handling cyclical object structures.
+  *
+  */
+
+  const deepEquals = (apples, oranges) => {
+    if (apples === oranges) {
+      return true;
+    }
+    if ((apples && !oranges) || (!apples && oranges)) {
+      return false;
+    }
+    if (!(apple instanceof Object) || !(orange instanceof Object)) {
+      return false;
+    }
+    let appleKeys = Object.keys(apples);
+    let orangeKeys = Object.keys(oranges);
+    if (appleKeys.length !== orangeKeys.length) {
+      return false;
+    }
+    if (appleKeys.length === 0) {
+      return true; // two empty objects are equal
+    }
+    for (let i = 0; i < appleKeys.length; i++) {
+      if (!deepEquals(apple[appleKeys[i]], orange[orangeKeys[i]])) {
+        return false;
+      }
+    }
+    return true;
+  }
