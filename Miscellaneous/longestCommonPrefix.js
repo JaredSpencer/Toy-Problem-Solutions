e432084abc5812721d26a466ed5364d691281c56
JARED SPENCER <jareddespencer@gmail.com>
2017-05-24 12:08:44 -0700

    refactored longestCommonPrefix to have a faster runtime.

--- Miscellaneous/longestCommonPrefix.js
+++ Miscellaneous/longestCommonPrefix.js
@@ -6,16 +6,13 @@ var longestCommonPrefix = array => {
   });
   let shortest = array[0];
   let shortSub;
-  for (let i = shortest.length; i >= 0; i--) {
-    shortSub = shortest.substring(0, i)
+  for (let i = 0; i < shortest.length; i++) {
+    shortSub = shortest.substring(0, i + 1);
     for (let j = 1; j < array.length; j++) {
       if (!array[j].includes(shortSub)) {
-        break;
-      }
-      if (j === array.length - 1) {
-        return shortSub;
+        return shortSub.substring(0, shortSub.length - 1);
       }
     }
   }
-  return null;
+  return '';
 }
