fca18b91d65836d0151176e8e5bbd2acc6451b13
JARED SPENCER <jareddespencer@gmail.com>
2017-05-31 14:09:07 -0700

    added allAnagrams. debugged and refactored. Working properly.

--- Arrays & Strings/allAnagrams.js
+++ Arrays & Strings/allAnagrams.js
@@ -1,5 +1,16 @@
 
 
 const allAnagrams = word => {
-  
+  let possibilities = {};
+
+  let anagram = (ana, str) => {
+    if (str === '') {
+      possibilities[ana] = 1;
+    }
+    for (let i = 0; i < str.length; i++) {
+      anagram(ana + str.charAt(i), str.slice(0, i) + str.slice(i + 1))
+    }
+  }
+  anagram('', word);
+  return Object.keys(possibilities);
 }
