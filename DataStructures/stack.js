e6a972da43e7642a34f6675d5915bcb7c5261965
JARED SPENCER <jareddespencer@gmail.com>
2017-06-12 18:41:45 -0700

    implemented es6 Stack class using array as storage.

--- DataStructures/stack.js
+++ DataStructures/stack.js
@@ -18,6 +18,30 @@ class Stack {
       let toReturn = this.storage[this.count];
       delete this.storage[this.count];
       return toReturn;
+    } else {
+      return 'stack is empty';
+    }
+  }
+}
+
+class Stack {
+  constructor() {
+    this.storage = [];
+    this.count = 0;
+  }
+  length() {
+    return this.count;
+  }
+  push(value) {
+    this.storage.push(value);
+    this.count++;
+  }
+  pop() {
+    if (this.count > 0) {
+      this.count--;
+      return this.storage.pop();
+    } else {
+      return 'stack is empty';
     }
   }
 }
