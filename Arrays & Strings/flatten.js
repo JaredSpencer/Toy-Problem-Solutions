a0510fb966eaffda345c94a348ad3c827c789083
JARED SPENCER <jareddespencer@gmail.com>
2017-07-08 14:38:19 -0700

    redid flatten from memory

--- Arrays & Strings/flatten.js
+++ Arrays & Strings/flatten.js
@@ -44,3 +44,16 @@ const flatten = array => {
   }
   return result;
 }
+
+const flatten = array => {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      result = result.concat(flatten(array[i]));
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
